3049b93d6ac5ce7964d4ff3a8d5045374551d1c0
Dmitriy Dultsev <tsukikohaya@gmail.com>
2023-10-13 15:17:28 +0300

    delete catch in auth service

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,18 +1,29 @@
 import { NextFunction, Request, Response } from "express";
 import { CustomHttpError } from "../errors/customHttpError";
 import { MulterError } from "multer";
+import { CustomRepositoryError } from "../errors/customRepositoryError";
 
 export function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
+   // HTTP
    if (err instanceof CustomHttpError) {
       res.status(err.status).json({ error: err.message }).end();
       return;
+      // MulterError
    } else if (err instanceof MulterError) {
       switch (err.code) {
          case "LIMIT_FILE_SIZE":
             res.status(413).json({ error: err.code });
             break;
       }
-      return
+      return;
+      // REPOSITORY
+   } else if (err instanceof CustomRepositoryError) {
+      if (err.status < 500) {
+         res.status(err.status).json({ error: err.message }).end();
+      } else {
+         res.status(err.status).json({ error: "Internal server error" }).end();
+      }
+      return;
    }
    console.error(err.message);
    res.status(500).json({ msg: "Internal server error" });
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -12,50 +12,34 @@ export class AuthService implements AuthServiceInterface {
    constructor(private jwtService: JWTService, private userRepository: UserRepository) {}
 
    async createUser(dto: SignupDto) {
-      try {
-         //gen hash
-         const salt = await bcrypt.genSalt(10);
-         const hashedPassword = await bcrypt.hash(dto.password, salt);
-         dto.password = hashedPassword;
+      //gen hash
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(dto.password, salt);
+      dto.password = hashedPassword;
 
-         //create user
-         const user = await this.userRepository.create(dto);
-         //create token
-         const token = this.jwtService.createToken(dto.email);
-         return token;
-      } catch (error) {
-         if (error instanceof CustomRepositoryError) {
-            throw new CustomHttpError(error.message, error.status);
-         } else {
-            throw new Error();
-         }
-      }
+      //create user
+      const user = await this.userRepository.create(dto);
+      //create token
+      const token = this.jwtService.createToken(dto.email);
+      return token;
    }
 
    async checkCredentials(dto: LoginDto): Promise<string> {
-      try {
-         // find user
-         const user = await this.userRepository.findOne(dto.email);
-         if (!user) {
-            throw new CustomHttpError("User not found", 403);
-         }
-
-         // check hash
-         const verifyPwd = await bcrypt.compare(dto.password, user.hash);
-         if (!verifyPwd) {
-            throw new CustomHttpError("User not found", 403);
-         }
-
-         // create token
-         const token = this.jwtService.createToken(dto.email);
-         return token;
-      } catch (error) {
-         if (error instanceof CustomHttpError) {
-            throw new CustomHttpError("Incorrect credentials", 403);
-         } else {
-            throw new Error("Something went wrong");
-         }
+      // find user
+      const user = await this.userRepository.findOne(dto.email);
+      if (!user) {
+         throw new CustomHttpError("User not found", 403);
       }
+
+      // check hash
+      const verifyPwd = await bcrypt.compare(dto.password, user.hash);
+      if (!verifyPwd) {
+         throw new CustomHttpError("User not found", 403);
+      }
+
+      // create token
+      const token = this.jwtService.createToken(dto.email);
+      return token;
    }
 }
 
